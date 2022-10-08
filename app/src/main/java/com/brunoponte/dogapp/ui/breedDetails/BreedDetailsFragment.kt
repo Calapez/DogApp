802757b6c291fa229de2735d5ecf3137b321b599
Bruno Ponte <brunocalapez@hotmail.com>
2022-10-09 00:42:44 +0100

    Implement Breed Details

--- app/src/main/java/com/brunoponte/dogapp/ui/breedDetails/BreedDetailsFragment.kt
+++ app/src/main/java/com/brunoponte/dogapp/ui/breedDetails/BreedDetailsFragment.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.brunoponte.dogapp.R
 import com.brunoponte.dogapp.databinding.FragmentBreedDetailsBinding
+import com.brunoponte.dogapp.domainModels.Breed
+import com.brunoponte.dogapp.helpers.Util
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,10 +20,7 @@ class BreedDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentBreedDetailsBinding
     private val viewModel: BreedDetailsViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val args: BreedDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +28,41 @@ class BreedDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBreedDetailsBinding.inflate(inflater, container, false)
-        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val breedId = args.breedId
+        viewModel.getBreedFromId(breedId)
+
+        setupViewModelObservers()
+    }
+
+    private fun setupViewModelObservers() {
+        viewModel.selectedBreed.observe(viewLifecycleOwner) { breed ->
+            setupView(breed)
+        }
+    }
+
+    private fun setupView(breed: Breed?) {
+        breed?.let {
+            val notApplicableText = getString(R.string.not_applicable)
+
+            Glide.with(this)
+                .load(String.format(Util.dogImageApiUrlTemplate, breed.referenceImageId))
+                .into(binding.breedImage)
+
+            binding.breedNameText.text = breed.name ?: notApplicableText
+
+            binding.breedGroupText.text = breed.breedGroup ?: notApplicableText
+
+            binding.breedOriginText.text = breed.origin ?: notApplicableText
+
+            binding.breedTemperamentText.text = breed.temperament ?: notApplicableText
+        }
+
+    }
 }
--- app/src/main/java/com/brunoponte/dogapp/ui/breedDetails/BreedDetailsViewModel.kt
+++ app/src/main/java/com/brunoponte/dogapp/ui/breedDetails/BreedDetailsViewModel.kt
@@ -1,8 +1,13 @@
 package com.brunoponte.dogapp.ui.breedDetails
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.brunoponte.dogapp.domainModels.Breed
 import com.brunoponte.dogapp.repository.IBreedRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +16,14 @@ class BreedDetailsViewModel
 constructor(
     private val breedRepository: IBreedRepository
 ) : ViewModel() {
+
+    val selectedBreed: MutableLiveData<Breed?> = MutableLiveData(null)
+
+    fun getBreedFromId(breedId: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val breed = breedRepository.getBreed(breedId)
+            selectedBreed.postValue(breed)
+        }
+    }
 
 }
